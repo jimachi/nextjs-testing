35a497c3425fcac7ba6548acafeb433e401ee057
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-26 02:10:20 +0900

    feat: ブログページの実装

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,58 @@
+import { FC } from 'react'
+import Link from 'next/link'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import Layout from '../../components/Layout'
+import { getAllPostsIds, getPostData } from '../../lib/fetch'
+import { POST } from '../../types/Types'
+
+const PostDetail: FC<POST> = ({ id, title, body }) => {
+  return (
+    <Layout title={title}>
+      <p className="m-4">
+        {'ID : '}
+        {id}
+      </p>
+      <p className="mb-4 text-xl font-bold">{title}</p>
+      <p className="mx-10 mb-12">{body}</p>
+      <Link href="/blog-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            className="w-6 h-6 mr-3"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+            ></path>
+          </svg>
+          <a data-testid="back-blog">Back to blog-page</a>
+        </div>
+      </Link>
+    </Layout>
+  )
+}
+
+export default PostDetail
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getAllPostsIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const post = await getPostData(ctx.params.id as string)
+  return {
+    props: {
+      post,
+    },
+  }
+}
